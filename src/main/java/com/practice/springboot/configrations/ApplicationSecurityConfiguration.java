ed3c9403fdeda7b6ed1a6eb63f28da28cbcefa41
the-5ultan <rfgamers2005@gmail.com>
2026-07-31 21:15:08 +0500

    Configured the secuity layer halfway

---
+++ src/main/java/com/practice/springboot/configrations/ApplicationSecurityConfiguration.java
@@ -0,0 +1,56 @@
+package com.practice.springboot.configrations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.proxy.NoOp;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfiguration {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//        http
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/", "/login", "/register").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .formLogin(Customizer.withDefaults());
+//
+//        return http.build();
+//    }
+
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        List<UserDetails> users = new ArrayList<>();
+//        users.add(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+//        return new InMemoryUserDetailsManager(users);
+//    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userDetailsService);
+        authenticationProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        return  authenticationProvider;
+    }
+
+
+}
