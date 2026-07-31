8df64f0a2aefc301c1c7fc1f4916b303163a817e
the-5ultan <rfgamers2005@gmail.com>
2026-07-31 13:02:17 +0500

    security implemented on  the homepage for test

---
+++ src/main/java/com/practice/springboot/controllers/AlienController.java
@@ -0,0 +1,13 @@
+package com.practice.springboot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AlienController {
+
+    @GetMapping(path="/")
+    public String home(){
+        return "index";
+    }
+}
