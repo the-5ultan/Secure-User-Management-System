797e90ab1fec0c3e1f3bf229e18b91f0e961fa7b
the-5ultan <rfgamers2005@gmail.com>
2026-08-07 20:21:15 +0500

    adding user service layer

---
+++ src/main/java/com/practice/springboot/services/MyUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.practice.springboot.services;
+
+import com.practice.springboot.entities.User;
+import com.practice.springboot.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return null;
+    }
+}
